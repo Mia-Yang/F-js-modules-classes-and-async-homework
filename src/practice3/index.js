4455bc89cad0a33fddca1f066438756052faa585
Yang Siyu <siyuyang1995@gmail.com>
2020-09-15 17:30:32 +0800

    [yang siyu]feat: pass the practice3

--- src/practice3/index.js
+++ src/practice3/index.js
@@ -1,1 +1,9 @@
-export const getPoetry
+export const getPoetry = async () => {
+    const url = "https://v1.jinrishici.com/all.json";
+
+    return fetch(url)
+    .then((response) => response.json())
+    .then(data => {
+        return [data.origin, data.author, data.content]
+    })
+}
